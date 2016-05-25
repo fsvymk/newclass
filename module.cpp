64aca3a03057581ad65a1838bab28471b5a94d9b
Mihail <fsvymk@gmail.com>
2016-05-25 22:20:23 +0300

    module class 1.0

--- module.cpp
+++ module.cpp
@@ -4,3 +4,10 @@ module::module()
 {
 
 }
+
+void module::compile(){
+    this->compiled.clear();
+    QByteArray *R = &this->compiled;
+
+    R->append("\n\n");
+}
--- module.h
+++ module.h
@@ -1,11 +1,18 @@
 #ifndef MODULE_H
 #define MODULE_H
 
+#include <QApplication>
 
 class module
 {
 public:
+    QStringList code;
+    QByteArray  compiled;
     module();
+
+    // methods:
+
+    void compile();
 };
 
 #endif // MODULE_H
