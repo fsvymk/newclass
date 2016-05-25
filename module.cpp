b83639631d4fdc89e4e977fb61a4c59501fd999c
Mihail <fsvymk@gmail.com>
2016-05-25 22:37:59 +0300

    class module 1.0.1

--- module.cpp
+++ module.cpp
@@ -1,4 +1,5 @@
 #include "module.h"
+#include "vartypes.h"
 
 module::module()
 {
@@ -10,4 +11,20 @@ void module::compile(){
     QByteArray *R = &this->compiled;
 
     R->append("\n\n");
+}
+
+void module::prepareVariables(){
+    QStringList::iterator it;
+    VarTypes VT;
+    QRegExp testDefinition(VT.getRegExpQueue() + "[\\s\\t]*([\\w\\d\\_]*)");
+
+    for(it=this->code.begin(); it!=this->code.end(); ++it){
+        int test = testDefinition.indexIn(*it);
+        if(test >= 0){
+            QString type = testDefinition.cap(1);
+            QString defs = testDefinition.cap(2);
+
+
+        }
+    }
 }
--- module.h
+++ module.h
@@ -2,17 +2,20 @@
 #define MODULE_H
 
 #include <QApplication>
+#include <variable.h>
 
 class module
 {
 public:
-    QStringList code;
-    QByteArray  compiled;
+    QStringList         code;
+    QByteArray          compiled;
+    QList<variable>     variables;
     module();
 
     // methods:
 
     void compile();
+    void prepareVariables();
 };
 
 #endif // MODULE_H
