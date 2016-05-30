d39a4ab8bad12ab164df0c388f07535267a4491a
Mihail <fsvymk@gmail.com>
2016-05-30 06:02:20 +0300

    once more try

--- module.cpp
+++ module.cpp
@@ -272,13 +272,20 @@ void module::compile(){
     prepareProcedures();
 
     collectHeader();
-
-    QList<procedure>::iterator PROC;
-    for(PROC = this->procedures.begin(); PROC != this->procedures.end(); ++PROC){
-       PROC->compile();
-    }
-
     this->collectA6();
     this->compiled.append(this->blockA6);
+
+    // QList<procedure>::iterator PROC;
+    // for(PROC = this->procedures.begin(); PROC != this->procedures.end(); ++PROC){
+    //    PROC->compile();
+    //    this->compiled.append(PROC->byteBody);
+    // }
+
+    int n = this->procedureCount;
+    int i = 0;
+    for(i=0; i<n; i++){
+        this->compiled.append(this->procedures.at(i).byteBody);
+    }
+
     this->toHex();
 }
